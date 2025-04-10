4c497aa1429c9f70efc269f9d870b656463cf571
DillonIs <dillonis826@gmail.com>
2025-04-10 22:57:19 +0000

    Added a function that finds the depth of each node

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+size_t min(size_t a, size_t b);
+/**
+ * binary_tree_depth - Finds the depth of each node
+ * @tree: Pointer to root node
+ *
+ * Return: Returns depth
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+	binary_tree_t *current = NULL;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	
+	current = tree->parent;
+
+	while (current)
+	{
+		depth += 1;
+		current = current->parent;
+	}
+	return (depth);
+}
